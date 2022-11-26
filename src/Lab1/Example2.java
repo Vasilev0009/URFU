e6199032e5d2fb884feaca4574b3e6bcd90291df
Vasilev0009 <ben00000009@gmail.com>
2022-11-26 13:46:04 +0500

    commit

---
+++ src/Lab1/Example2.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+public class Example2 {
+
+    public static void main(String[] args) {
+        int num = 100;
+        System.out.println("num: " + num);
+        num = num * 2;
+
+        System.out.println("num * 2 = " + num);
+    }
+}
