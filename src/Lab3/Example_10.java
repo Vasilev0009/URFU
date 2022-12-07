d66caa652786b7970a77991d5745127b0a86f0f6
Vasilev0009 <ben00000009@gmail.com>
2022-12-07 22:58:48 +0500

    commit

--- src/Lab3/Example_10.java
+++ src/Lab3/Example_10.java
@@ -1,6 +1,18 @@
 package Lab3;
-//Напишите программу, в которой создается целочисленный
-//массив, заполняется случайными числами и после этого значения элементов в
-//массиве сортируются в порядке убывания значений.
+
+import java.util.Arrays;
+import java.util.Random;
+
+//Напишите программу, в которой создается целочисленный массив, заполняется случайными числами
+// и после этого значения элементов в массиве сортируются в порядке убывания значений.
 public class Example_10 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        byte[] bytes = new byte[10];
+        random.nextBytes(bytes);
+        Arrays.sort(bytes);
+        for (int i = 0; i < bytes.length; i++) {
+            System.out.print(bytes[i] + " ");
+        }
+    }
 }
