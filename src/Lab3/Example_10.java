5837dd741ca109f483a775cad5ad4cef8de115df
Vasilev0009 <Vasilev00000009@gmail.com>
2022-12-08 15:34:13 +0500

    commit

--- src/Lab3/Example_10.java
+++ src/Lab3/Example_10.java
@@ -1,6 +1,5 @@
 package Lab3;
 
-import java.util.Arrays;
 import java.util.Random;
 
 //Напишите программу, в которой создается целочисленный массив, заполняется случайными числами
@@ -8,10 +7,16 @@ import java.util.Random;
 public class Example_10 {
     public static void main(String[] args) {
         Random random = new Random();
-        byte[] bytes = new byte[10];
+        byte[] bytes = new byte[15];
         random.nextBytes(bytes);
-        Arrays.sort(bytes);
         for (int i = 0; i < bytes.length; i++) {
+            for (int k = i + 1; k < bytes.length; k++){
+                if (bytes[k] > bytes[i]){
+                    byte temp = bytes[i];
+                    bytes[i] = bytes[k];
+                    bytes[k] = temp;
+                }
+            }
             System.out.print(bytes[i] + " ");
         }
     }
--- src/Lab3/Example_9.java
+++ src/Lab3/Example_9.java
@@ -1,7 +1,26 @@
 package Lab3;
+
+import java.util.Random;
+
 //Напишите программу, в которой создается массив и заполняется случайными числами.
 // Массив отображается в консольном окне.В этом массиве необходимо определить элемент с минимальным значением.
 // В частности, программа должна вывести значение элемента с минимальным значением и индекс этого элемента.
 // Если элементов с минимальным значением несколько, должны быть выведены индексы всех этих элементов.
 public class Example_9 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        byte[] bytes = new byte[15];
+        random.nextBytes(bytes);
+        byte min = bytes[0];
+        System.out.println("Элементы массива : ");
+        for (byte aByte : bytes) {
+            if (min > aByte) min = aByte;
+            System.out.print(aByte + " ");
+        }
+        System.out.println("\nНаименьшее значение элемента массива равно : " + min);
+        System.out.print("Индексы элементов с наименьшим значением: ");
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] == min) System.out.print("[" + i + "] ");
+        }
+    }
 }
